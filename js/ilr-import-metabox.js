9438a052957560974622e0dce90307aa4f858609
Matt Loar <matt@loar.name>
2016-11-13 18:50:08 -0600

    Auto-fill title, author, excerpt

--- js/ilr-import-metabox.js
+++ js/ilr-import-metabox.js
@@ -2,7 +2,21 @@ function handleFileSelect(evt) {
     var files = evt.target.files;
     var reader = new FileReader();
     docx(files[0]).then(function (r) {
-        r.mainDocument.childNodes.forEach(function (q) { document.getElementById("content").value += q.outerHTML; }); 
+        r.mainDocument.childNodes.forEach(function (q) {
+            if (q.className == 'pt-Head1-Articles') {
+                var titleNode = document.getElementById("title");
+                title.focus();
+                title.value = q.innerHTML;
+                title.blur();
+            } else if (q.className == 'pt-AuthorName1-Articles') {
+                document.getElementById("acf-field-ilr_author").value = q.textContent;
+            } else if (q.className == 'pt-Abstract') {
+                document.getElementById("excerpt").value += q.outerHTML;
+                document.getElementById("content").value += q.outerHTML; 
+            } else {
+                document.getElementById("content").value += q.outerHTML; 
+            }
+        }); 
         r.footnotes.childNodes.forEach(function (q) { document.getElementById("content").value += q.outerHTML; });
     });
 }
