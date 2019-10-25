678816c92f291316e9fc655ea7aea9e0dbfb4feb
Matt Loar <matt@loar.name>
2019-10-24 23:38:04 -0500

    All kinds of improvements

--- js/docx.js
+++ js/docx.js
@@ -92,11 +92,6 @@ function convertContent(input) { 'use strict'; // Convert HTML to Wordprocessing
             if (styleAttrNode = inNode.getElementsByTagName('jc')[0]) { outNode.style.textAlign = styleAttrNode.getAttribute('w:val'); }
             if (styleAttrNode = inNode.getElementsByTagName('pStyle')[0]) { outNode.className = 'pt-' + styleAttrNode.getAttribute('w:val'); }
             if (styleAttrNode = inNode.getElementsByTagName('color')[0]) { outNode.style.color = '#' + styleAttrNode.getAttribute('w:val'); }
-            if (styleAttrNode = inNode.getElementsByTagName('numPr')[0]) {
-                var lvl = styleAttrNode.getElementsByTagName('ilvl')[0].getAttribute('w:val');
-                var numId = styleAttrNode.getElementsByTagName('numId')[0].getAttribute('w:val');
-                outNode.className += " numbering-" + numId + '-' + lvl;
-            }
         }
         if (inNode.nodeName == 'hyperlink') {
             var id = inNode.getAttribute('r:id');
@@ -116,12 +111,42 @@ function convertContent(input) { 'use strict'; // Convert HTML to Wordprocessing
             outNode.name = name;
         }
         if (inNode.nodeName === 'r') {
+            var styleNode;
+            if (styleNode = inNode.getElementsByTagName('rStyle')[0]) {
+                outNode = outNode.appendChild(newHTMLnode('SPAN'));
+                outNode.className = 'pt-' + styleNode.getAttribute('w:val');
+            }
             outNode.innerHTML += processRun(inNode, state);
         }
     }
     function processPara(node, state, output) {
-        var outNode = output.appendChild(newHTMLnode('P'));;
-        var inNode, j, styleAttrNode;
+        var numNode, outNode = output, inNode, j, styleAttrNode, lvl, num;
+        if (numNode = node.getElementsByTagName('numPr')[0]) {
+            lvl = numNode.getElementsByTagName('ilvl')[0].getAttribute('w:val');
+            num = numNode.getElementsByTagName('numId')[0].getAttribute('w:val');
+        } else if (inNode = node.getElementsByTagName('pStyle')[0]) {
+            lvl = "0";
+            num = state.styleNums[inNode.getAttribute('w:val')];
+        }
+        if (lvl && num) {
+            var list = output.querySelector('#list-' + num + '-' + lvl);
+            if (!list) {
+                if (state.numbering.abstracts[state.numbering.nums[num].abstractId][lvl].numFmt == 'disc') {
+                    list = output.appendChild(newHTMLnode('UL'));
+                } else {
+                    list = output.appendChild(newHTMLnode('OL'));
+                    list.style['list-style-type'] = state.numbering.abstracts[state.numbering.nums[num].abstractId][lvl].numFmt;
+                }
+                list.id = 'list-' + num + '-' + lvl;
+            } else {
+                while (inNode = list.nextSibling) {
+                    inNode.parentNode.removeChild(inNode);
+                    list.appendChild(inNode);
+                }
+            }
+            outNode = list.appendChild(newHTMLnode('LI'));
+        }
+        outNode = outNode.appendChild(newHTMLnode('P'));;
         for (j = 0; inNode = node.childNodes[j]; j++) {
             processEtc(inNode, state, outNode);
         }
@@ -169,10 +194,10 @@ function convertContent(input) { 'use strict'; // Convert HTML to Wordprocessing
                 }
             }
 
-            return output;
+            return {"part": "docRels", "content": output};
         }));
         promises.push(input.files['word/styles.xml'].async("string").then(function (data) {
-            var output, inputDoc, i, j, k, id, doc, inNode, inNodeChild, outNode, outNodeChild, styleAttrNode, footnoteNode, pCount = 0, tempNode, val;
+            var output, inputDoc, i, j, k, id, doc, inNode, inNodeChild, outNode, outNodeChild, styleAttrNode, footnoteNode, pCount = 0, tempNode, val, styleNums = {};
             inputDoc = toXML(data);
             output = newHTMLnode('STYLE');
             for (i = 0; inNode = inputDoc.childNodes[i]; i++) {
@@ -192,10 +217,13 @@ function convertContent(input) { 'use strict'; // Convert HTML to Wordprocessing
                             if (styleAttrNode = inNodeChild.getElementsByTagName('u')[0]) { 
                                 output.appendChild(document.createTextNode('text-decoration: ' + (styleAttrNode.getAttribute('w:val') === 'none' ? 'none' : 'underline') + ';'));
                             }
-                            if (styleAttrNode = inNodeChild.getElementsByTagName('b')[0]) { 
-                                output.appendChild(document.createTextNode('font-weight: ' + (styleAttrNode.getAttribute('w:val') === 'off' ? 'normal' : (!!styleAttrNode.getAttribute('w:val') ? 'bold' : 'normal')) + ';'));
+                            if (styleAttrNode = inNodeChild.getElementsByTagName('b')[0]) {
+                                val = styleAttrNode.getAttribute('w:val');
+                                output.appendChild(document.createTextNode('font-weight: ' + (val === null ? 'bold' : (!!val ? 'bold' : 'normal')) + ';'));
                             }
-
+                            if (styleAttrNode = inNodeChild.getElementsByTagName('numId')[0]) {
+                                styleNums[inNode.getAttribute('w:styleId')] = styleAttrNode.getAttribute('w:val');
+                            }
                         }
                         if (inNodeChild.nodeName === 'rPr') {
                             if (inNodeChild.getElementsByTagName('smallCaps').length) {
@@ -210,19 +238,66 @@ function convertContent(input) { 'use strict'; // Convert HTML to Wordprocessing
                             if (styleAttrNode = inNodeChild.getElementsByTagName('u')[0]) { 
                                 output.appendChild(document.createTextNode('text-decoration: ' + (styleAttrNode.getAttribute('w:val') === 'none' ? 'none' : 'underline') + ';'));
                             }
-                            if (styleAttrNode = inNodeChild.getElementsByTagName('b')[0]) { 
-                                output.appendChild(document.createTextNode('font-weight: ' + (styleAttrNode.getAttribute('w:val') === 'off' ? 'normal' : (!!styleAttrNode.getAttribute('w:val') ? 'bold' : 'normal')) + ';'));
+                            if (styleAttrNode = inNodeChild.getElementsByTagName('b')[0]) {
+                                val = styleAttrNode.getAttribute('w:val');
+                                output.appendChild(document.createTextNode('font-weight: ' + (val === null ? 'bold' : (!!val ? 'bold' : 'normal')) + ';'));
                             }
                             if (styleAttrNode = inNodeChild.getElementsByTagName('color')[0]) {
-                                output.appendChild(document.createTextNode('color: #' + styleAttrNode.getAttribute('w:val')));
+                                output.appendChild(document.createTextNode('color: #' + styleAttrNode.getAttribute('w:val') + ';'));
                             }
                         }
                     }
                     output.appendChild(document.createTextNode("}\r\n"));
                 }
             }
-            return output;
+            output = {"stylesheet" : output, "styleNums": styleNums};
+            return {"part": "styles", "content": output};
         }));
+        if ('word/numbering.xml' in input.files) {
+            promises.push(input.files['word/numbering.xml'].async("string").then(function (data) {
+                function translateFormat(fmt) {
+                    switch (fmt) {
+                        case 'bullet':
+                            return 'disc';
+                        case 'upperLetter':
+                            return 'upper-latin';
+                        case 'lowerLetter':
+                            return 'lower-latin';
+                        case 'decimal':
+                            return 'decimal';
+                        case 'upperRoman':
+                            return 'upper-roman';
+                        case 'lowerRoman':
+                            return 'lower-roman';
+                    }
+                }
+                var output = {"abstracts": {}, "nums": {}}, inputDoc, h, i, j, k, id, doc, fnNode, inNode, inNodeChild, outNode, outNodeChild, styleAttrNode, footnoteNode, pCount = 0, tempStr, tempNode, val;
+                inputDoc = toXML(data);
+                for (h = 0; fnNode = inputDoc.childNodes[h]; h++) {
+                    if (fnNode.nodeName === 'abstractNum') {
+                        var abstractId = fnNode.getAttribute('w:abstractNumId');
+                        output.abstracts[abstractId] = {};
+                        for (i = 0; inNode = fnNode.childNodes[i]; i++) {
+                            if (inNode.nodeName === 'lvl') {
+                                var lvl = inNode.getAttribute('w:ilvl');
+                                output.abstracts[abstractId][lvl] = {};
+                                for (j = 0; inNodeChild = inNode.childNodes[j]; j++) {
+                                    if (inNodeChild.nodeName === 'numFmt') {
+                                        output.abstracts[abstractId][lvl].numFmt = translateFormat(inNodeChild.getAttribute('w:val'));
+                                    }
+                                }
+                            }
+                        }
+                    } else if (fnNode.nodeName === 'num') {
+                        inNode = fnNode.childNodes[0];
+                        var numId = fnNode.getAttribute('w:numId');
+                        var abstractId = inNode.getAttribute('w:val');
+                        output.nums[numId] = {"abstractId": abstractId};
+                    }
+                }
+                return {"part": "numbering", "content": output};
+            }));
+        }
         if ('word/footnotes.xml' in input.files) {
             promises.push(input.files['word/footnotes.xml'].async("string").then(function (data) {
                 var output, inputDoc, h, i, j, k, id, doc, fnNode, inNode, inNodeChild, outNode, outNodeChild, styleAttrNode, footnoteNode, pCount = 0, tempStr, tempNode, val;
@@ -248,19 +323,19 @@ function convertContent(input) { 'use strict'; // Convert HTML to Wordprocessing
                         }
                     }
                 }
-                return output;
+                return {"part": "footnotes", "content": output};
             }));
         }
         return Promise.all(promises).then(function (results) {
-            var ret = {
-                docRels: results[0],
-                styles: results[1],
-                footnotes: results[2]
-            };
+            var ret = {};
+            results.forEach(function (d) {
+                ret[d.part] = d.content;
+            });
             return input.files['word/document.xml'].async("string").then(function (data) {
-                var output, inputDoc, i, j, k, id, doc, inNode, inNodeChild, outNode, outNodeChild, styleAttrNode, footnoteNode, pCount = 0, tempNode, val, state = {"footnoteId": {"value": 1}, "listLevel": -1, "hyperlinks": ret.docRels.hyperlinks};
+                var output, inputDoc, i, j, k, id, doc, inNode, inNodeChild, outNode, outNodeChild, styleAttrNode, footnoteNode, pCount = 0, tempNode, val, state = {"footnoteId": {"value": 1}, "numbering": ret.numbering, "styleNums": ret.styles.styleNums, "hyperlinks": ret.docRels.hyperlinks};
                 inputDoc = toXML(data).getElementsByTagName('body')[0];
                 output = newHTMLnode('DIV');
+                console.log(inputDoc);
                 for (i = 0; inNode = inputDoc.childNodes[i]; i++) {
                     if (inNode.nodeName == 'p') {
                         processPara(inNode, state, output);
